ad87e5af5c1cc5bafaa72dfaa583fb6ab7500f5e
npldevfr <asnpldev@gmail.com>
2023-11-27 17:03:55 +0100

    fix: favicon

--- app.vue
+++ app.vue
@@ -3,6 +3,13 @@ useHead({
   htmlAttrs: {
     class: ' scroll-smooth font-inter',
   },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg',
+    },
+  ],
 })
 </script>
 
